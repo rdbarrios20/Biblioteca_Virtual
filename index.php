e8f60d7558c58747434ab32ad8f7db697583c8cf
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-04 15:51:24 -0500

    Texto registro lirbo

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <a class="nav-item nav-link active" href="index.php">Inicio<span class="sr-only">(current)</span></a>
-                        <a class="nav-item nav-link active" href="registro_libro.php">Registro</a>
+                        <a class="nav-item nav-link active" href="registro_libro.php">Registro Libro</a>
                         <a class="nav-item nav-link active" href="inventario.php">Inventario</a>
                     </div>
                 </div>
