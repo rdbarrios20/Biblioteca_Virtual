336c74f238fdada3e3624edf113a90287b62a112
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-05 22:02:05 -0500

    Script terminado para modificar

--- php/modificar.php
+++ php/modificar.php
@@ -1,12 +1,33 @@
 <?php require_once("../php/databaseConnection.php")?>
 
 <?php
+if (isset($_POST['codigo']) && ($_POST['nombre_libro'] != "")) {
+    $codigo = $_POST['codigo'];
+    $autor = $_POST['autor'];
+    $nombre_libro = $_POST['nombre_libro'];
+    $fecha_expedicion = $_POST['fecha_expedicion'];
+    $disponibilidad = $_POST['disponibilidad'];
+    $precio_publico = $_POST['precio_publico'];
+    $precio_interno = $_POST['precio_interno'];
+    $reservado = $_POST['reservado'];
+    $cantidad = $_POST['cantidad'];
 
-    $ide=$_POST['ide_libro'];
-    //$query="DELETE FROM libros WHERE CODIGO_LIBRO ='$ide'";
+    $modificar = ("UPDATE libros SET AUTOR=('" . $autor . "'),
+    NOMBRE_LIBRO=('" . $nombre_libro . "'),FECHA_EXPEDICION=('" . $fecha_expedicion . "'),
+    DISPONIBILIDAD=('" . $disponibilidad . "'),PRECIO_PUBLICO=('" . $precio_publico . "'),
+    PRECIO_INTERNO=('" . $precio_interno . "'),RESERVADO=('" . $reservado . "'),
+    CANTIDAD=('" . $cantidad . "') WHERE CODIGO_LIBRO='" . $codigo . "'");
 
-    $result = $connection->query($query);
+    $consulta = mysqli_query($connection, $modificar, $resultmode = MYSQLI_STORE_RESULT);
+    mysqli_select_db($connection,$database);
 
-    echo "Registro eliminado con exito";
+    if ($consulta == false) {
+        die('No se puedo ejecutar la consulta');
+    }
 
-?>
+    $msg = "Datos modificados exitosamente";
+    $response = array('success' => true, 'message' => $msg);
+    header('content-type: application/json'); // This line makes no difference.
+    echo  json_encode($response);
+
+}
