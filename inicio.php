4405f29ec997a4a463bc9721a43aa767ac342b02
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-12 16:11:51 -0500

    Change directory name index for inicio

---
+++ inicio.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="Estilos/styles.css">
+
+    <script src="Scripts/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="Scripts/busqueda.js"></script>
+    <script src="popper/popper.min.js"></script>
+    <title>Inicio</title>
+</head>
+
+<body>
+    <div class="container">
+        <br>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Biblioteca</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="inicio.php">Inicio<span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link active" href="registro_libro.php">Registro Libro</a>
+                        <a class="nav-item nav-link active" href="inventario.php">Inventario</a>
+                        <a class="nav-item nav-link active" href="index.php">Salir</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </div>
+    <section>
+        <div class="container">
+            <img src="Images/libros.jpg" alt="">
+        </div>
+    </section>
+    <footer></footer>
+</body>
+
+</html>
