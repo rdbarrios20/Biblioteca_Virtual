c24c019e850ff8135a89a8e0cf295f8896c99c83
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-16 18:44:58 -0500

    Modify location redirection session

--- registro_libro.php
+++ registro_libro.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['usuario'])) {
         header('location: index.php');
     }
 } else {
-    header('location: inventario.php');
+    header('location: registro_libro.php');
 }
 ?>
 <!DOCTYPE html>
