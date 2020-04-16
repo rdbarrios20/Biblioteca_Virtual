f4265e68939bea2e82da63e8f5c2e5f7f528cdde
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-15 20:16:29 -0500

    Script login validation js

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,23 +1,33 @@
 $(document).on('submit','#login-form', function (event) {
     debugger
+    event.preventDefault();
     $.ajax({
         url: 'php/login.php',
         type: 'POST',
         dataType: 'json',
-        data: {
-            user: $('usser').val(),
-            pasword: $('pasword').val()
-        },
-
-        success: function (response) {
-            if (response == true) {
-                console.log(response);
-            }
-        },
-
-        error: function (response) {
-            console.log(response);
+        data: $(this).serialize(),
+        beforeSend:function(){
+            $('.login').val('Validando....');
         }
     })
-    event.preventDefault();
+    .done(function(response){
+        console.log(response);
+        if(!response.validation){
+            if(response.tipo=='Administrador'){
+                location.href='inicio.php';
+            }else{
+                $('.error').slideDown('slow');
+                setTimeout(function(){
+                    $('.error').slideUp('slow');
+                },3000);
+                $('.login').val('Iniciar sesión');
+            }
+        }
+    })
+    .fail(function(error){
+        console.log(error.responseText);
+    })
+    .always(function(){
+        console.log("complete");
+    });
 });
