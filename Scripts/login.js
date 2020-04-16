137df766fbe09e131b4b700b6a35802abd8f8987
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-15 22:01:31 -0500

    Undo changes login js

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,33 +1,33 @@
-$(document).on('submit','#login-form', function (event) {
+$(document).on('submit', '#login-form', function(event) {
     debugger
     event.preventDefault();
     $.ajax({
-        url: 'php/login.php',
-        type: 'POST',
-        dataType: 'json',
-        data: $(this).serialize(),
-        beforeSend:function(){
-            $('.login').val('Validando....');
-        }
-    })
-    .done(function(response){
-        console.log(response);
-        if(!response.validation){
-            if(response.tipo=='Administrador'){
-                location.href='inicio.php';
-            }else{
-                $('.error').slideDown('slow');
-                setTimeout(function(){
-                    $('.error').slideUp('slow');
-                },3000);
-                $('.login').val('Iniciar sesión');
+            url: 'php/login.php',
+            type: 'POST',
+            dataType: 'json',
+            data: $(this).serialize(),
+            beforeSend: function() {
+                $('.login').val('Validando....');
             }
-        }
-    })
-    .fail(function(error){
-        console.log(error.responseText);
-    })
-    .always(function(){
-        console.log("complete");
-    });
+        })
+        .done(function(response) {
+            console.log(response);
+            if (!response.validation) {
+                if (response.tipo == 'Administrador') {
+                    location.href = 'inicio.php';
+                } else {
+                    $('.error').slideDown('slow');
+                    setTimeout(function() {
+                        $('.error').slideUp('slow');
+                    }, 3000);
+                    $('.login').val('Iniciar sesión');
+                }
+            }
+        })
+        .fail(function(error) {
+            console.log(error.responseText);
+        })
+        .always(function() {
+            console.log("complete");
+        });
 });
