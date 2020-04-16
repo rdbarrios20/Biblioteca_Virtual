f31c1281fef4acf75b5f0bbe54a2e938521b1725
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-15 20:18:00 -0500

    Script login validation php

--- php/login.php
+++ php/login.php
@@ -1,16 +1,13 @@
 <?php require_once("../php/databaseConnection.php") ?>
 
 <?php
-if (isset($_POST['usser']) && ($_POST['pasword'] != '')) {
-    $result =$mysqli->query("SELECT nombre_apellido, tipo_usuario FROM usuarios WHERE nombre_apellido ='" . $_POST['usser'] . "' AND pasword='" . $_POST['pasword'] . "'");
-
-    if ($result->num_rows == 1) {
-        $datos = $result->fetch_assoc();
-        echo json_encode(array('error' => false, 'tipo' => $datos['tipo_usuario']));
-    } else {
-        echo json_encode(array('error' => true));
-    }
+$result = $connection->query("SELECT nombre_apellido,tipo_usuario FROM usuarios WHERE usuario ='" . $_POST['user'] . "' AND pasword='" . $_POST['pasword'] . "'");
+if ($result->num_rows == 1) {
+    $datos = $result->fetch_assoc();
+    echo json_encode(array('validation' => false, 'tipo' => $datos['tipo_usuario']));
+} else {
+    $message="Usuario o Contraseña errados";
+    echo json_encode(array('validation' => true,'message'=>$message));
 }
-
 $connection->close();
 ?>
