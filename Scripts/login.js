278b6e45a0f99b639371f3aae6bb211f927a4d1e
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-17 20:26:59 -0500

    Add loader scripts to index

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,18 +1,18 @@
-$(document).on('submit', '#login-form', function(event) {
-    debugger
+$(document).on('submit', '#login-form', function (event) {
     event.preventDefault();
     $.ajax({
-            url: 'php/login.php',
-            type: 'POST',
-            dataType: 'json',
-            data: $(this).serialize(),
-            beforeSend: function() {
-                $('.login').val('Validando....');
-            }
-        })
-        .done(function(respuesta) {
+        url: 'php/login.php',
+        type: 'POST',
+        dataType: 'json',
+        data: $(this).serialize(),
+        beforeSend: function () {
+            $('.loading-gear').html('<br><div class="loading"><img src="images/large-ajax-loader.gif" alt="loading" /><br/>Validando</div>');
+        }
+    })
+        .done(function (respuesta) {
             if (!respuesta.validation) {
                 if (respuesta.tipo == 'Administrador') {
+                    $('.loading').fadeIn(3000).html(respuesta);
                     location.href = 'inicio.php';
                 } else {
                     if (respuesta.tipo == '');
@@ -20,16 +20,18 @@ $(document).on('submit', '#login-form', function(event) {
                 }
             } else {
                 $('.error').slideDown('slow');
-                setTimeout(function() {
+                setTimeout(function () {
+                    $('.loading-gear').fadeIn(3000).html(respuesta);
                     $('.error').slideUp('slow');
                 }, 3000);
                 $('.login').val('Iniciar sesión');
+                $('.loading-gear').fadeOut(3000).html(respuesta);
             }
         })
-        .fail(function(resp) {
+        .fail(function (resp) {
             console.log(resp.responseText);
         })
-        .always(function() {
+        .always(function () {
             console.log("complete");
         });
 });
