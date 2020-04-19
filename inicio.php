2ec6b444a5a6dafe52ebb5738684caa3235f2fcd
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-19 00:47:14 -0500

    Add warning icon

--- inicio.php
+++ inicio.php
@@ -18,6 +18,7 @@ if (isset($_SESSION['usuario'])) {
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="Estilos/styles.css">
     <link rel="stylesheet" href="Estilos/loader.css">
+    <link rel="stylesheet" href="fonts/style.css">
 
     <script src="Scripts/jquery-3.4.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -28,6 +29,10 @@ if (isset($_SESSION['usuario'])) {
     <script type="text/javascript">
         $(document).ready(function() {
             $('.loading2').fadeOut(2000).html();
+
+            $('.salir').on('click', function() {
+                location.href = 'salir.php';
+            })
         });
     </script>
 </head>
@@ -46,7 +51,7 @@ if (isset($_SESSION['usuario'])) {
                         <a class="nav-item nav-link active" href="inicio.php">Inicio<span class="sr-only">(current)</span></a>
                         <a class="nav-item nav-link active" href="registro_libro.php">Registro Libro</a>
                         <a class="nav-item nav-link active" href="inventario.php">Inventario</a>
-                        <a class="nav-item nav-link active" href="salir.php">Salir</a>
+                        <a class="nav-item nav-link active" data-toggle='modal' data-target='#close_page' href="">Salir</a>
                     </div>
                 </div>
             </nav>
@@ -55,11 +60,29 @@ if (isset($_SESSION['usuario'])) {
     <section>
         <br>
         <div class="container">
+            <h5 class="bg-dark"> Usuario: <?php echo $_SESSION['usuario']['nombre_apellido'] ?></h5>
+        </div>
+        <br>
+        <div class="container">
             <h1 class="bg-dark">Biblioteca Virtual</h1>
             <img src="Images/libros.jpg" alt="">
         </div>
-        <div class="container">
-            <h4 class="bg-dark"> Bienvenido: <?php echo $_SESSION['usuario']['nombre_apellido'] ?></h4>
+        <!-- Modal Para salir de la pagina de inicio-->
+        <div class="modal" tabindex="-1" role="dialog" id="close_page">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title"><span class="icon-warning ico-warning"></h5>
+                    </div>
+                    <div class="modal-body">
+                        <p>Realmente desea salir?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-info salir" data-dismiss="modal">Aceptar</button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close">Cancelar</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     <footer></footer>
