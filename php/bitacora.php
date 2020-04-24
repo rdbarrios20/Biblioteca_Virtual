aec867572b87eadd6f3594dad7daea300f4eba19
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-04-23 22:08:04 -0500

    Proof state of push modifications

---
+++ php/bitacora.php
@@ -0,0 +1,13 @@
+<?php
+  require_once('../php/databaseConnection.php');
+  $connection->set_charset('utf8');
+
+  $rol="Administrador";
+  $id_user="1";
+  $accion="modificacion";
+  $detalles="Se modifico el registro 2089 las aventuras de felipe";
+
+  
+  $query
+
+?>
