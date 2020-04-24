f97601ee74d1c9af0c93a42e0d9e55850054d54f
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-04-23 22:25:26 -0500

    Keep modifying

--- php/bitacora.php
+++ php/bitacora.php
@@ -5,9 +5,13 @@
   $rol="Administrador";
   $id_user="1";
   $accion="modificacion";
+  $fecha="2020/03/22";
   $detalles="Se modifico el registro 2089 las aventuras de felipe";
 
   
-  $query
+  $query=$connection->prepare("INSERT INTO bitacora (rol,id_usuario,accion,fecha,detalle) VALUES
+  ('".$rol."','".$id_user."','".$accion."','".$fecha."','".$detalle."')");
+
+  
 
 ?>
