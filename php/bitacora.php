f71ae8e2fc1ca9f244eb4b9e10324498a68317d2
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-04-24 20:09:36 -0500

    Prepare the script for the variables

--- php/bitacora.php
+++ php/bitacora.php
@@ -1,25 +1,28 @@
 <?php
+    //funcion para insertar datos a la tabla bitacora.
     function insert_bitacora(){
-        require_once('../php/databaseConnection.php');
+        require_once('php\databaseConnection.php');
         $connection->set_charset('utf8');
       
         date_default_timezone_set('America/Bogota');
-        $rol="Administrador";
-        $id_user="1";
-        $accion="modificacion";
-        $fecha=date('y-m-d H:i:s'); 
-        $detalle="Se modifico el registro 2089 las aventuras de felipe";
+        // $rol="Administrador";
+        // $id_user="1";
+        // $accion="modificacion";
+         $fecha_creacion=date('y-m-d H:i:s'); 
+        // $detalle="Se modifico el registro 2089 las aventuras de felipe";
       
-        $query=$connection->prepare("INSERT INTO bitacora (rol,id_usuario,accion,fecha,detalle) VALUES
-        ('".$rol."','".$id_user."','".$accion."','".$fecha."','".$detalle."')");
+        $query=$connection->prepare("INSERT INTO bitacora (rol,id_usuario,accion,fecha_creacion,detalle) VALUES
+        ('".$rol."','".$id_user."','".$accion."','".$fecha_creacion."','".$detalle."')");
         
         $query->execute();
       
         $connection->close();
     }
     
+ 
+    //Funcion para leer los datos ubicados en la tabla bitacora.
     function read_bitacora(){
-        require_once('../php/databaseConnection.php');
+        require_once('php\databaseConnection.php');
         $connection->set_charset('utf8');
     
         $query=$connection->prepare("SELECT * FROM bitacora ORDER BY id");
