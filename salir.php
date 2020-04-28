86f86e1bcd41e89a28e8b905172f0f63506a1f00
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-04-27 22:05:12 -0500

    Add dates for registri exit in bitacora

--- salir.php
+++ salir.php
@@ -1,5 +1,11 @@
 <?php
+    include("./php/bitacora.php");
     session_start();
+    $rol=$_SESSION['usuario']['tipo_usuario'];
+    $id_usuario=$_SESSION['usuario']['id_usuario'];
+    $accion="Cerrar Sesión";
+    $detalle="Cerro Sesión";
+    insert_bitacora($rol,$id_usuario,$accion,$detalle);
 
     session_destroy();
 
