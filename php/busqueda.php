1e4de89135263e2495ce21bfa6a0ca9478d384cb
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-04-27 22:08:44 -0500

    Include function connection required

--- php/busqueda.php
+++ php/busqueda.php
@@ -1,39 +1,39 @@
-<?php require_once("../php/databaseConnection.php") ?>
-
 <?php
+    require("databaseConnection.php");
+    $connection=OpenCon();
+    $q = $_POST['texto'];
+        $query = '';
 
-$q = $_POST['texto'];
-$query = '';
-
-if (isset($q) && $q != '') {
-    //Executar la consulta con criterios
-    $query = "SELECT CODIGO_LIBRO,AUTOR,NOMBRE_LIBRO,FECHA_EXPEDICION,DISPONIBILIDAD,PRECIO_PUBLICO,PRECIO_INTERNO,RESERVADO,CANTIDAD FROM LIBROS WHERE CODIGO_LIBRO LIKE '%" . $q . "%' OR NOMBRE_LIBRO LIKE '%" . $q . "%' ";
-} else {
-    //Executar la consulta sin criterios
-    $query = "SELECT * FROM LIBROS ORDER BY CODIGO_LIBRO";
-}
+    if (isset($q) && $q != '') {
+        //Executar la consulta con criterios
+        $query = "SELECT CODIGO_LIBRO,AUTOR,NOMBRE_LIBRO,FECHA_EXPEDICION,DISPONIBILIDAD,PRECIO_PUBLICO,PRECIO_INTERNO,RESERVADO,CANTIDAD FROM LIBROS WHERE CODIGO_LIBRO LIKE '%" . $q . "%' OR NOMBRE_LIBRO LIKE '%" . $q . "%' ";
+    } else {
+        //Executar la consulta sin criterios
+        $query = "SELECT * FROM LIBROS ORDER BY CODIGO_LIBRO";
+    }
 
 
-$result = $connection->query($query);
-$arrayList = [];
-$isSuccess = false; //VARAIBALE QUE VAMOS A USAR PARA INDICARLE AL FRONT SI EXISTEN COICIDENCIAS O NO
+    $result = $connection->query($query);
+    $arrayList = [];
+    $isSuccess = false; //VARAIBALE QUE VAMOS A USAR PARA INDICARLE AL FRONT SI EXISTEN COICIDENCIAS O NO
 
-if (isset($result->num_rows) &&  $result->num_rows > 0) {
+    if (isset($result->num_rows) &&  $result->num_rows > 0) {
 
-     while ($item = $result->fetch_assoc()) {
-        array_push($arrayList, $item);
+         while ($item = $result->fetch_assoc()) {
+            array_push($arrayList, $item);
 
-     }
+        }
      $isSuccess = true;
    
-} 
+    } 
 
-// Version: Responsidento en formato JSON
-$response = array('success' => $isSuccess, 'result' => $arrayList);
-header('content-type: application/json'); // This line makes no difference.
-echo json_encode($response);
+    // Version: Responsidento en formato JSON
+    $response = array('success' => $isSuccess, 'result' => $arrayList);
+    header('content-type: application/json'); // This line makes no difference.
+    echo json_encode($response);
 
 
-// Cerrar conection al final
-$connection->close();
+    // Cerrar conection al final
+    CloseCon($connection);
+
 ?>
