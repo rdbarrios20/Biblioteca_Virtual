ed4f0849459d707ae45ee62d29a78c7b4eae811f
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-05-04 20:11:44 -0500

    Add elemts to bitacora in search script.php

--- php/busqueda.php
+++ php/busqueda.php
@@ -6,8 +6,17 @@
     $criterio = $_POST['texto'];
 
     if(isset($criterio) && !empty($criterio)){
-        $query = $connection->prepare("SELECT * FROM libros WHERE CODIGO_LIBRO LIKE '%$criterio%' OR NOMBRE_LIBRO LIKE '%$criterio%'");
+        $query = $connection->prepare("SELECT * FROM libros WHERE CODIGO_LIBRO 
+        LIKE '%$criterio%' OR NOMBRE_LIBRO LIKE '%$criterio%'");
         $query->execute();
+        
+        session_start();
+        $rol=$_SESSION['usuario']['tipo_usuario'];
+        $id_usuario=$_SESSION['usuario']['id_usuario'];
+        $accion="Consulta";
+        $detalle="Consulta libro";
+        insert_bitacora($rol,$id_usuario,$accion,$detalle);
+
     }else{
         $query = $connection->prepare("SELECT * FROM libros WHERE CODIGO_LIBRO ORDER BY CODIGO_LIBRO DESC");
         $query->execute();
@@ -22,13 +31,6 @@
         }
         $success = true;
     }
-
-    session_start();
-    $rol=$_SESSION['usuario']['tipo_usuario'];
-    $id_usuario=$_SESSION['usuario']['id_usuario'];
-    $accion="Consulta";
-    $detalle="Consulta libro";
-    insert_bitacora($rol,$id_usuario,$accion,$detalle);
 
     CloseCon($connection);
 
